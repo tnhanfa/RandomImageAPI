ef6b0a983fb60ad5f17a384613921643c3159b75
Nguyễn Thiện Nhân <88951651+tnhanfa@users.noreply.github.com>
2022-07-14 14:42:22 +0700

    Update

--- controllers/cadao.js
+++ controllers/cadao.js
@@ -3,5 +3,7 @@ const { errorHandler } = require("../utils");
 exports.cadao = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../text/cadao.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Authors":"tnhan","url":`${link}`});
+  res.json({"author":"tnhan",
+  "count":`${data.length}`,
+  "data":`${link}`});
 };
